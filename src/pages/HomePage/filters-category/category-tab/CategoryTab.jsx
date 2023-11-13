33ef2ef0fa3177d0ca4409d725fc216651b983dc
Frontend <sweetysks23@gmail.com>
2023-11-13 20:42:51 +0900

    메인page로직 및 필터기능 구현

--- src/pages/HomePage/filters-category/category-tab/CategoryTab.jsx
+++ src/pages/HomePage/filters-category/category-tab/CategoryTab.jsx
@@ -1,7 +1,26 @@
 import styles from "./CategoryTab.module.scss";
+import { setActiveCategory } from "../../../../store/categories/categories.slice";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/redux";
 
-const CategoryTab = () => {
-  return <button className={styles.active_category}>텍스트</button>;
+const CategoryTab = ({ text, categoryName }) => {
+  const dispatch = useAppDispatch();
+  const category = useAppSelector((state) => state.categoriesSlice);
+
+  const getActiveCategory = () => {
+    dispatch(setActiveCategory(categoryName));
+  };
+  return (
+    <button
+      className={
+        categoryName === category // redux store에서 가져온 category
+          ? styles.active_category
+          : styles.category_button
+      }
+      onClick={getActiveCategory}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default CategoryTab;
