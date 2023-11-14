28071b979c320227276ef5cc319c4699f2090963
Frontend <sweetysks23@gmail.com>
2023-11-14 13:31:47 +0900

    장바구니기능구현

--- src/pages/CartPage/cart-list/cart-item/CartItem.jsx
+++ src/pages/CartPage/cart-list/cart-item/CartItem.jsx
@@ -1,25 +1,52 @@
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../../hooks/redux";
+import {
+  decrementProduct,
+  deleteFromCart,
+  incrementProduct,
+} from "../../../../store/cart/cart.slice";
 import styles from "./CartItem.module.scss";
 import { AiOutlineDelete } from "react-icons/ai";
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const dispatch = useAppDispatch();
+
+  const deleteProduct = () => {
+    dispatch(deleteFromCart(item.id));
+  };
+
+  const incrementCount = () => {
+    dispatch(incrementProduct(item.id));
+  };
+
+  const decrementCount = () => {
+    dispatch(decrementProduct(item.id));
+  };
   return (
     <div className={styles.cart_item}>
-      <img src="이미지" alt="product card" />
-
+      <Link to={`/product/${item.id}`}>
+        <img src={item.image} alt="product card" />
+      </Link>
       <div className={styles.cart_description}>
-        <h3>이미지카테고리</h3>
-        <h2>아이템타이틀</h2>
-        <span>아이템총가격</span>
+        <h3>{item.category}</h3>
+        <h2>{item.title}</h2>
+        <span>
+          {item.price} X {item.quantity} = ${item.total.toFixed(2)}
+        </span>
       </div>
 
       <div className={styles.cart_count}>
         <div>
-          <button>-</button>
-          <span>아이템량</span>
-          <button>+</button>
+          <button disabled={item.quantity === 1} onClick={decrementCount}>
+            -
+          </button>
+          <span>{item.quantity}</span>
+          <button disabled={item.quantity === 10} onClick={incrementCount}>
+            +
+          </button>
         </div>
       </div>
-      <button className={styles.cart_delete}>
+      <button className={styles.cart_delete} onClick={deleteProduct}>
         <AiOutlineDelete />
       </button>
     </div>
