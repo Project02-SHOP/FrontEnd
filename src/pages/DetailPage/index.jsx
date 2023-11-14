da77942e828888815065347e7b056078bc3e9cfc
Jihyeong Han <hjh000504@naver.com>
2023-11-14 15:59:57 +0900

    디테일페이지 작업 실시

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,38 +1,65 @@
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import styles from "./DetailPage.module.scss";
+import Loader from "../../components/loader/Loader";
+import { addToCart } from "../../store/cart/cart.slice";
+import { fetchProduct } from "../../store/products/product.slice";
 
 const DetailPage = () => {
+  const { id } = useParams();
+  const productId = Number(id);
+  const dispatch = useAppDispatch();
+
+  const { product, isLoading } = useAppSelector((state) => state.productSlice);
+  const { products } = useAppSelector((state) => state.cartSlice);
+  const productMatching = products.some((product) => product.id === product.id);
+
+  useEffect(() => {
+    dispatch(fetchProduct(productId));
+  }, [productId]);
+
+  const addItemToCart = () => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <div className="page">
-      {/* <Loader /> */}
-
-      <div className={styles.card_wrapper}>
-        <div className={styles.card_img}>
-          <img src="/img/detail_img.jpg" alt="product card" />
-          <ul className={styles.card_thumb}>
-            <li>11</li>
-            <li>22</li>
-            <li>33</li>
-          </ul>
-        </div>
-
-        <div className={styles.card_description}>
-          <h3>제품카테고리</h3>
-          <h1>제품타이틀</h1>
-
-          <h4> $ 제품가격</h4>
-          <p>제품설명</p>
-          <div className={styles.option}>
-            <div className={styles.option_sel}></div>
-            <div className={styles.option_sel}></div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={styles.card_wrapper}>
+          <div className={styles.card_img}>
+            <img src={product.image} alt="product card" />
+            <ul className={styles.card_thumb}>
+              <li>{product.image[0]}</li>
+              <li>{product.image[1]}</li>
+              <li>{product.image[2]}</li>
+            </ul>
           </div>
-          <div>
-            <button>장바구니에 담기</button>
-            장바구니로 이동
+
+          <div className={styles.card_description}>
+            <h3>{product.category}</h3>
+            <h1>{product.title}</h1>
+            <h4> $ {product.price}</h4>
+            <p>{product.description}</p>
+            <div className={styles.option}>
+              <div className={styles.option_sel}></div>
+              <div className={styles.option_sel}></div>
+            </div>
+            <div>
+              <button
+                disabled={!productMatching}
+                onClick={() => productMatching && addItemToCart()}
+              >
+                {productMatching ? "장바구니에 담기" : "장바구니에 담긴 제품"}
+              </button>
+              <Link to={"/cart"}>장바구니로 이동</Link>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
-
 export default DetailPage;
