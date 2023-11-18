782d39a608d08d0c7b9a4a32af77813db33fff30
Jihyeong Han <hjh000504@naver.com>
2023-11-18 16:48:23 +0900

    vite.config.js 수정

--- src/store/additional/additional.slice.js
+++ src/store/additional/additional.slice.js
@@ -1,11 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../../shared/apis/Apis";
+import axios from "axios";
 
 export const createProduct = createAsyncThunk(
   "product/createProduct",
-  async (thunkAPI) => {
+  async (product, thunkAPI) => {
     try {
-      const response = await api.post("/api/sell/create");
+      const response = await axios.post("/api/product/create", product, {
+        withCredentials: true,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue("Error creating product");
