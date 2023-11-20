790fcb1341677c53d4c44e96d1b6333c06037dd3
Frontend <sweetysks23@gmail.com>
2023-11-20 15:56:43 +0900

    코드정리

--- src/store/user/user.slice.js
+++ src/store/user/user.slice.js
@@ -13,78 +13,13 @@ const initialState = {
 export const loadTokenFB = createAsyncThunk(
   "user/loadToken",
   async (_, { dispatch }) => {
-    if (getCookie("Authorization")) {
-      dispatch(loadToken());
+    const token = getCookie("Authorization");
+    if (token) {
+      dispatch(loadToken(token));
     }
   }
 );
 
-// // 로그인 비동기 액션
-// export const loginDB = createAsyncThunk(
-//   "user/login",
-//   async ({ email, password }, { dispatch }) => {
-//     try {
-//       const response = await api.post("/api/user/login", {
-//         email: email,
-//         password: password,
-//       });
-//       dispatch(login({ is_login: true, token: response.data.token }));
-//       setCookie("Authorization", response.data.token);
-//       setCookie("nickname", response.data.nickname);
-//       setCookie("profileimage", response.data.profileimage);
-//       setCookie("email", response.data.email);
-//       setCookie("password", response.data.password)
-//       return response.data;
-//     } catch (error) {
-//       window.alert("로그인 에러");
-//       console.error("Login Error", error);
-//     }
-//   }
-// );
-
-// const userSlice = createSlice({
-//   name: "user",
-//   initialState,
-//   reducers: {
-//     login: (state, action) => {
-//       setCookie("is_login", "true");
-//       state.token = action.payload.token;
-//       state.user = action.payload.user;
-//       state.is_login = true;
-//     },
-//     logOut: (state) => {
-//       deleteCookie("is_login");
-//       localStorage.removeItem("nickname");
-//       localStorage.removeItem("token");
-//       state.user = null;
-//       state.is_login = false;
-//     },
-//     loadToken: (state) => {
-//       const token = getCookie("Authorization");
-//       if (token) {
-//         state.is_login = true;
-//         state.token = token;
-//       }
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder.addCase(loginDB.fulfilled, (state, action) => {
-//       state.is_login = true;
-//       state.token = action.payload.token;
-//     });
-//     builder.addCase(loadTokenFB.fulfilled, (state, action) => {
-//       state.is_login = true;
-//       state.token = action.payload.token;
-//     });
-//   },
-// });
-
-// export const { login, logOut, loadToken } = userSlice.actions;
-
-// // Export the reducer
-// export default userSlice.reducer;
-
-// 로그인 비동기 액션
 export const loginDB = createAsyncThunk(
   "user/login",
   async ({ email, password }, { dispatch }) => {
@@ -136,12 +71,9 @@ const userSlice = createSlice({
       state.userInfo = null;
       state.is_login = false;
     },
-    loadToken: (state) => {
-      const token = getCookie("Authorization");
-      if (token) {
-        state.is_login = true;
-        state.token = token;
-      }
+    loadToken: (state, action) => {
+      state.is_login = true;
+      state.token = action.payload;
     },
   },
   extraReducers: (builder) => {
@@ -152,12 +84,11 @@ const userSlice = createSlice({
     });
     builder.addCase(loadTokenFB.fulfilled, (state, action) => {
       state.is_login = true;
-      state.token = action.payload.token;
+      state.token = action.payload;
     });
   },
 });
 
 export const { login, logOut, loadToken } = userSlice.actions;
 
-// Export the reducer
 export default userSlice.reducer;
